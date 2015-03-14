1e4574c455c0fe63b89507f591e374d55409259e
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2015-03-14 14:59:57 +0000

    retrieving a game

--- src/test/java/com/stratified/tennis/dao/GameDaoTest.java
+++ src/test/java/com/stratified/tennis/dao/GameDaoTest.java
@@ -9,8 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {Database.class, GameBackendBeans.class})
@@ -28,11 +27,17 @@ public class GameDaoTest {
 	}
 
 	@Test
-	public void retrieve() {
+	public void retrievePositive() {
 		Game game = Game.newGame("p1", "p2");
 		Game created = gameDao.create(game);
 
 		Game retrieved = gameDao.retrieve(created.getId());
 		assertEquals(created, retrieved);
 	}
+
+	@Test
+	public void retrieveNegative() {
+		assertNull(gameDao.retrieve(6));
+	}
+
 }
